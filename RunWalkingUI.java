85009ad5db146a15fb6ee2aab2b2770d9fb39c68
llonaheinrich55-cyber <llonaheinrich55@gmail.com>
2025-11-01 16:48:45 +0800

    Delete RunWalkingUI.java

--- RunWalkingUI.java
+++
@@ -1,9 +0,0 @@
-package fitrak3.ui.basic;
-
-import javax.swing.*;
-
-public class RunWalkingUI {
-    public static void main (String [] args){
-       SwingUtilities.invokeLater(() -> new WalkingWorkoutCalculator());
-    }
-}
